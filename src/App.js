6b6e29a56d0936bf0bfd9b4d40041c44d4b61415
nandasrimogili <105092803+nandasrimogili@users.noreply.github.com>
2022-09-30 09:21:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ChangeColors from "./components/ChangeColors.js";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
+
+export default App;
